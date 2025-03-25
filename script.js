bfe3ecb8522b2d75676a5e519fe2b5562be9ca43
Shonibare Kareem Addisa <generousska@gmail.com>
2025-03-24 22:46:50 -0700

    New Changes

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// Form Submission Handling
 document
   .getElementById("contact-form")
   .addEventListener("submit", function (e) {
     e.preventDefault();
     alert("Thank you for your message! I will get back to you soon.");
-    // You can add AJAX or backend integration here
   });
